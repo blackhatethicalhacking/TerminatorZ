0f73c5d9506aad3e6a7ad099ad3392f328a249b5
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-16 06:21:38 +0200

    Update TerminatorZ.sh

--- TerminatorZ.sh
+++ TerminatorZ.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+curl --silent "https://raw.githubusercontent.com/blackhatethicalhacking/Subdomain_Bruteforce_bheh/main/ascii.sh" | lolcat
+echo ""
+# Generate a random Sun Tzu quote for offensive security
+# Array of Sun Tzu quotes
+quotes=("The supreme art of war is to subdue the enemy without fighting." "All warfare is based on deception." "He who knows when he can fight and when he cannot, will be victorious." "The whole secret lies in confusing the enemy, so that he cannot fathom our real intent." "To win one hundred victories in one hundred battles is not the acme of skill. To subdue the enemy without fighting is the acme of skill.")
+# Get a random quote from the array
+random_quote=${quotes[$RANDOM % ${#quotes[@]}]}
+# Print the quote
+echo "Offensive Security Tip: $random_quote - Sun Tzu" | lolcat
+sleep 1
+echo "MEANS, IT'S ☕ 1337 ⚡ TIME, 369 ☯ " | lolcat
+sleep 1
+figlet -w 80 -f small TerminatorZ | lolcat
+echo ""
+echo "[YOUR ARE USING TerminatorZ.sh] - (v1.0) CODED BY Chris 'SaintDruG' Abou-Chabké WITH ❤ FOR blackhatethicalhacking.com for Educational Purposes only!" | lolcat
+sleep 1
+#check if the user is connected to the internet
+tput bold;echo "CHECKING IF YOU ARE CONNECTED TO THE INTERNET!" | lolcat
+# Check connection
+wget -q --spider https://google.com
+if [ $? -ne 0 ];then
+    echo "++++ CONNECT TO THE INTERNET BEFORE RUNNING TerminatorZ.sh!" | lolcat
+    exit 1
+fi
+tput bold;echo "++++ CONNECTION FOUND, LET'S GO!" | lolcat
 echo "Enter the domain: "
 read domain
 
