874807f286b6431f6c040f96726eca59701919c1
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-17 07:43:19 +0200

    Update TerminatorZ.sh

--- TerminatorZ.sh
+++ TerminatorZ.sh
@@ -24,6 +24,34 @@ if [ $? -ne 0 ];then
     exit 1
 fi
 tput bold;echo "++++ CONNECTION FOUND, LET'S GO!" | lolcat
+# Matrix effect
+echo "Entering the Matrix for 5 seconds in:" | toilet --metal -f term -F border
+sleep 1
+echo "3" | toilet --gay -f term -F border
+sleep 1
+echo "2" | toilet --metal -f term -F border
+sleep 1
+echo "1" | toilet --gay -f term -F border
+sleep 1
+
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[1;33m'
+B='\033[0;34m'
+P='\033[0;35m'
+C='\033[0;36m'
+W='\033[1;37m'
+
+for ((i=0; i<5; i++)); do
+    echo -ne "${R}10 ${G}01 ${Y}11 ${B}00 ${P}01 ${C}10 ${W}00 ${G}11 ${P}01 ${B}10 ${Y}11 ${C}00\r"
+    sleep 0.2
+    echo -ne "${R}01 ${G}10 ${Y}00 ${B}11 ${P}10 ${C}01 ${W}11 ${G}00 ${P}10 ${B}01 ${Y}00 ${C}11\r"
+    sleep 0.2
+    echo -ne "${R}11 ${G}00 ${Y}10 ${B}01 ${P}00 ${C}11 ${W}01 ${G}10 ${P}00 ${B}11 ${Y}10 ${C}01\r"
+    sleep 0.2
+    echo -ne "${R}00 ${G}11 ${Y}01 ${B}10 ${P}11 ${C}00 ${W}10 ${G}01 ${P}11 ${B}00 ${Y}01 ${C}10\r"
+    sleep 0.2
+done
 echo "Enter the domain: "
 read domain
 
